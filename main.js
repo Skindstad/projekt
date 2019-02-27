17faadb7c0153cfef7784ad74c254252afad9138
Unknown <clemmensen.maria@gmail.com>
2019-02-27 12:23:03 +0100

    Update for showing different sections

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	$("#sidebar li").click(function(){
 		$("#sidebar li").attr("class", "");
 		$(this).attr("class", "selected");
+
+		$("#content div").css("display", "none");
+		$("#content_" + this.getAttribute("href").substring(1)).css("display", "block");
 	});
 });
