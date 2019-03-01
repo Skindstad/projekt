55db9a32f045ea69bfcfe44bec780142a8b56405
Unknown <clemmensen.maria@gmail.com>
2019-03-01 10:32:35 +0100

    Departments fetch for insert
    Delete temp deactivated

--- Readme.txt
+++ Readme.txt
@@ -2,5 +2,15 @@ Gruppe projekt vedr. clientside programmering.
 Gruppemedlemmer: Jakob Skindstad Frederiksen og Maria Clemmensen
 
 Husk at installere node og derefter køre:
-npm install mysql
-npm install mime
+> npm install mysql
+> npm install mime
+
+Programmet startes ved at køde:
+> node PageSelector.js
+- og derefter besøge localhost:8001/
+
+Nyttige adresser:
+/query?select=SELECT%20sql%20statement
+/query?query=INSERT20%INTO%20statement
+/query?query=DELETE20%FROM%20statement
+/query?query=UPDATE20%TABLE%20statement
--- main.js
+++ main.js
@@ -38,19 +38,32 @@ $(document).ready(function () {
     $("#database_table").change(function () {
         var selected = $(this).children("option:selected").val().split("_");
         toggleInsert(selected);
+        if (selected[0]=="dept")
+            populateDepartments();
+
     });
 
+    
 
+    function populateDepartments() {
+        var select = $("#dept_no");
+        select.children().remove();
 
+        var query = "SELECT dept_no, dept_name FROM departments";
+        $.getJSON("/query?select=" + encodeURIComponent(query), function (data) {
 
+            $.each(data, function (key, val) {
+                var newOpt = new Option(val["dept_name"], val["dept_no"]);
+                select.append(newOpt);
+            });
+        });
+    }
 
-
-
-
+    /*
 $("#dept_no").change(function(){
 	var query = encodeURIComponent("SELECT dept_no FROM departments");
 	toggleInsert("<option value='", query, "'>", query,"</option>");
-})
+})*/
 
 
 
@@ -184,13 +197,6 @@ function emp(data,querystring,querystring2) {
 
 
 
-
-
-
-
-
-
-
     /* Design    */
     var designColumns = {
         showEmployees: {
@@ -203,19 +209,87 @@ function emp(data,querystring,querystring2) {
             hire_date : "Ansat:"
         },
 
+        showDepartments: {
+            edit_delete: "",
+            dept_no: "Afdelings nr.:",
+            dept_name: "Afdelings navn:"
+        },
+
+        showDeptManager: {
+            edit_delete: "",
+            emp_no: "Medarbejder nr.:",
+            dept_no: "Afdelings nr.:",
+            from_date: "Fra dato:",
+            to_date: "Til dato:"
+        },
+
+        showTitles: {
+            edit_delete: "",
+            emp_no: "Medarbejder nr.:",
+            title: "Titel:",
+            from_date: "Fra dato:",
+            to_date: "Til dato:"
+        },
+
+        showDeptEmp: {
+            edit_delete: "",
+            emp_no: "Medarbejder nr.:",
+            dept_no: "Afdelings nr.:",
+            from_date: "Fra dato:",
+            to_date: "Til dato:"
+        },
+
+        showSalaries: {
+            edit_delete: "",
+            emp_no: "Medarbejder nr.:",
+            salary: "Løn:",
+            from_date: "Fra dato:",
+            to_date: "Til dato:"
+        },
+
         methods: {
+
+            /* For show employees */            
             birth_date: function (val) { return val.split("T")[0]; },
             hire_date: function (val) { return val.split("T")[0]; },
             edit_delete: function (val) {
                 return "<ul><li><input type=\"checkbox\"></li><li class=\"x\" title=\"Slet\">S</li><li class=\"e\" title=\"Ændre\">Æ</li></ul>";
-            }
+            },
+            gender: function (val) { return "<span class=\"" + val + "\">" + val + "</span>"; },
+            from_date: function (val) { return val.split("T")[0]; },
+            to_date: function (val) { return val.split("T")[0]; }            
         }
+    }
+
+
+
+    /* Delete single row */
+    function deleteRow(table, elm) {
+        var columnElements = $(elm).parents("div[class^='row']").children("div[class!='edit_delete']");
+        /*columnElements.each( => {
+            alert(this);
+        });*/
+
+        var identifier = row.children("div:nth-child(2)");
+        alert(row.html());
+
+        /*
+
+        var columnIdentifier = identifier.attr("class");
+        var columnValue = identifier.text().split(": ")[1];
+
+        var query = "DELETE FROM " + table + " WHERE " + columnIdentifier + "='" + columnValue + "' AND ";
+        */
+
+        //$.getJSON("/query?query=" + query);
+        //alert(query);
+       // $(row).fadeOut(2000, function(){ $(this).remove(); });
     }
     
 
 
     /* Insert the retrieved data */
-    function insertData(query, designIdentifier) {
+    function insertData(query, designIdentifier, table) {
         $.getJSON("/query?select=" + query, function (data) {
             var rows = [];
 
@@ -230,69 +304,70 @@ function emp(data,querystring,querystring2) {
                         if (designColumns["methods"][identifier])
                             rowValues[identifier] = designColumns["methods"][identifier](rowValues[identifier]);
 
-                        rowElement += "<div>" + value + " " + rowValues[identifier] + "</div>";
+                        rowElement += "<div class=\"" + identifier + "\">" + value + " " + rowValues[identifier] + "</div>";
                     });
                 else
                     $.each(rowValues, function (identifier, value) {
-                        rowElement += "<div>" + identifier + " " + value + "</div>";
+                        rowElement += "<div class=\"" + identifier + "\">" + identifier + " " + value + "</div>";
                     });
 
                 rows.push("<div class=\"row" + ((rows.length % 2) + 1) + "\">" + rowElement + "</div>");
             });
 
             $("#contentData_" + designIdentifier).html(rows.join(""));
+
+            /* Assign deletion method */
+            $("#contentData_" + designIdentifier + " .x").click(function () {
+                deleteRow(table, this);
+            })
         });
     }
 
 
 
     /* Show all employees example with input fields for string manipulation and example view */
-    function toggleShow(elmVisible, query, elmHref = elmVisible) {
+    function toggleShow(elmVisible, query, table, elmHref = elmVisible) {
         $("#content_show > div[class!='limiter']").css("display", "none");
         $("#content_" + elmVisible).css("display", "block");
 
         $("#content_show > ul > li").attr("class", "");
         $("li[href='?" + elmHref + "']").attr("class", "selected");
-        insertData(query, elmVisible);
+        insertData(query, elmVisible, table);
     }
 
     /* Show all employees */
     $("li[href='?show']").click(function (e) {
         var query = encodeURIComponent("SELECT * FROM employees LIMIT 10");
-        toggleShow("showEmployees", query, "show");
+        toggleShow("showEmployees", query, "employees", "show");
     });
 
     /* Show all departments */
     $("li[href='?showDepartments']").click(function (e) {
         var query = encodeURIComponent("SELECT * FROM departments");
-        toggleShow("showDepartments", query);
+        toggleShow("showDepartments", query, "departments");
     });
 
     /* Show manager of departments */
     $("li[href='?showDeptManager']").click(function (e) {
         var query = encodeURIComponent("SELECT * FROM dept_manager");
-        toggleShow("showDeptManager", query);
+        toggleShow("showDeptManager", query, "dept_manager");
     });
 
     /* Show titles */
     $("li[href='?showTitles']").click(function (e) {
         var query = encodeURIComponent("SELECT * FROM titles LIMIT 10");
-        toggleShow("showTitles", query);
+        toggleShow("showTitles", query, "titles");
     });
 
     /* Show department employees */
     $("li[href='?showDeptEmp']").click(function (e) {
         var query = encodeURIComponent("SELECT * FROM dept_emp LIMIT 10");
-        toggleShow("showDeptEmp", query);
+        toggleShow("showDeptEmp", query, "dept_emp");
     });
 
     /* Show salaries */
     $("li[href='?showSalaries']").click(function (e) {
         var query = encodeURIComponent("SELECT * FROM salaries LIMIT 10");
-        toggleShow("showSalaries", query);
+        toggleShow("showSalaries", query, "salaries");
     });
-
-
-				/* TODO : INSERT ... works now.
-	INSERT INTO departments(dept_no, dept_name) VALUES("d999", "Test");*/
 });
